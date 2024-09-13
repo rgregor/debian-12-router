df9fe59fa2997dc27449e033e6f7dd7b23eb2e92
RobertG <gimpel@gwire.de>
2024-09-13 12:52:35 +0200

    WIP: add pre-commit + linters... remove leftover files...

--- setup_router.sh
+++ setup_router.sh
@@ -2,18 +2,12 @@
 
 # run this script from the project root directory for the debian 12 router setup
 # make sure the SSH Agent is set up to provide the remote host keys
-# and the remote Debian host already has an appropriate SSH account (to be done manually 
+# and the remote Debian host already has an appropriate SSH account (to be done manually
 # and omitted here to reduce exposure of info that could compromise credential security)
 
-# also configure the bare Debian 12 installation to have ~/.ssh/authorized_keys setup, remotely
+[[ -f "$ANSIBLE_VAULT_PASSWORD_FILE" ]] || { echo "please configure your .env file correctly, refer to dotenv.template" ; exit 1; }
 
-playbook=playbooks/router/router.yaml
-extra_args=-vvv
+playbook=playbooks/forbidden-router/forbidden-router.yaml
+extra_args=-vv
 
-if [ -f ~/.ansible_router_vars ]; then
-    ansible-playbook $playbook --vault-password-file ~/.ansible_router_vars $extra_args
-else
-    ansible-playbook $playbook --ask-vault-pass $extra_args
-fi
-
-
+ansible-playbook $playbook $extra_args
